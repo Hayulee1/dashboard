9297d14a0439d728adca7fcf91b55c18fe49bf4f
Hayulee1 <135177814+Hayulee1@users.noreply.github.com>
2023-06-06 16:50:21 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ Created with ❤️ by [Data Professor](https://youtube.com/dataprofessor/).
 
 
 # Row A
-st.markdown('Metrics')
+st.markdown('### Metrics')
 col1, col2, col3 = st.columns(3)
 col1.metric("Temperature", "70 °F", "1.2 °F")
 col2.metric("Wind", "9 mph", "-8%")
